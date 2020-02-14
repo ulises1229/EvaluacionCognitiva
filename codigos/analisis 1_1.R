59f0bf9de33e6c8828a611e177eb2a73638695b6
DevAlexI <48767282+DevAlexI@users.noreply.github.com>
2020-02-14 11:17:51 -0600

    Cambios columnas y script en r

--- codigos/analisis 1_1.R
+++ codigos/analisis 1_1.R
@@ -5,19 +5,30 @@ library(Matrix)
 library(ggplot2)
 
 prueba <- read_csv('C:/Users/Alex Isasi/Documents/GitHub/EvaluacionCognitiva/Bases de datos/Primaria/Prueba 1/1_h_prim_todo_de_prueba1.csv')
+
+#reactivo_uno_resp_uno, #reactivo_uno_resp_dos
 resultados <- prueba %>%
-  select(reactivouno) %>%
-  count(reactivouno, sort = TRUE)
+  select(reactivo_tres) %>%
+  count(reactivo_tres, sort = TRUE)
 resultados
 
-# Creaci�n de datos para la gr�fica
-M <- as.vector(resultados$reactivouno) #valores de los resultados
-H <- as.vector(resultados$n) #veces en las que se repiten los resultados
-print(H)
-print(M)
+# Creaci???n de datos para la gr???fica
+new_res <- subset(resultados, n!=1)
+new_res_one <- subset(resultados, n == 1)
+new_res
+new_res_one
 
-p<-ggplot(data=resultados, aes(x=M, y=H, color = H)) +
+respuestas <- as.vector(new_res$reactivo_tres)
+
+frecuencias <- as.vector(new_res$n)
+
+
+p<-ggplot(data=new_res, aes(x=respuestas, y=frecuencias)) +
   geom_bar(stat="identity", fill = "steelblue") +
-  geom_text(aes(label = H), vjust = -0.3, size = 3.5) + 
+  geom_text(aes(label = frecuencias), vjust = -0.3, size = 3.5) + 
   theme_minimal()
 p
+
+
+
+
