0da528aaae6e2abe8404cd638bc8550f2ee9576c
DevAlexI <48767282+DevAlexI@users.noreply.github.com>
2020-05-15 13:58:14 -0500

    Conteo con clasificación terminado (código)

--- codigos/conteo_clasif_palabras.py
+++ codigos/conteo_clasif_palabras.py
@@ -3,43 +3,77 @@ import pandas as pd
 import string
 from hunspell import Hunspell
 import sys
-#import spacy
+import spacy
 
-def row_x_row(vec):
-    for row in vec:
-        cleaned_row = clean_row(row)
-        return cleaned_row
-
-def classifier(word):
-    list_of_adv = []
-    list_of_verb = []
-    list_of_adj = []
-    list_of_sust = []
-    pass
+def row_x_row(vector, column):
+    list_of_values = []
+    for row in vector:
+        if row == 'NS' or row == '0' or row == 'NN' or row == 'WW' or row == '-':
+            pass
+        else:
+            cleaned_row = clean_row(row)
+            print("Oración limpia: ",cleaned_row)
+            list_of_values.append(cleaned_row)
+            #print('Valores hasta ahora: ',list_of_values)
+    new_df['clean_'+column] = pd.Series(list_of_values)
+    
+def classifier(dataframe):
+    for col in cols:
+        sustantivos_count = []
+        verbos_count = []
+        adjetivos_count = []
+        adverbios_count = []
+        for row in dataframe[col]:
+            if row == 'NS' or row == '0' or row == 'NN' or row == 'WW' or row == '-':
+                sustantivos_count.append(0)
+                verbos_count.append(0)
+                adjetivos_count.append(0)
+                adverbios_count.append(0)
+            else:
+                sustantivos = []
+                verbos = []
+                adjetivos = []
+                adverbios = []
+                doc = nlp(row)
+                for word in doc:
+                    if word.pos_ == "NOUN":
+                        sustantivos.append(word.text)
+                    elif word.pos_ == "VERB":
+                        verbos.append(word.text)
+                    elif word.pos_ == "ADJ":
+                        adjetivos.append(word.text)
+                    elif word.pos_ == "ADV":
+                        adverbios.append(word.text)
+                sustantivos_count.append(len(sustantivos))
+                verbos_count.append(len(verbos))
+                adjetivos_count.append(len(adjetivos))
+                adverbios_count.append(len(adverbios))
+            df_sustantivos[col+'_cuenta_sustantivos'] = pd.Series(sustantivos_count)
+            df_verbos[col+'_cuenta_verbos'] = pd.Series(verbos_count)
+            df_adjetivos[col+'_cuenta_adjetivos'] = pd.Series(adjetivos_count)
+            df_adverbios[col+'_cuenta_adverbios'] = pd.Series(adverbios_count)
 
 def analyzer(strings_list):
-    new_sentence = " "
-    sentence = ''.join(strings_list)
+    sentence = ' '.join(strings_list)
+    print("analizando oración: {}".format(sentence))
     for string in strings_list:
-        print("analizando oración: {}".format(sentence))
         if h.spell(string) == True:
-            print("palabra: {}, es correcta.".format(string))
             pass
         elif h.spell(string) == False:
             print("palabra: {}, es incorrecta. Es necesario cambiarla".format(string))
             list_suggests = list(h.suggest(string))
             for i, value in enumerate(list_suggests, 0):
                 print("{} ->{}".format(i, value))
-            value = input("Elige la palabra que mejor se adapta o reescribe la oración:")
+            index = strings_list.index(string)
+            value = input("Elige la palabra que mejor se adapta o escribe la palabra correcta:")
             try:
                 val = int(value)
                 print("Has elegido la opción: {} ó -> {}".format(val, list_suggests[val]))
-                index = strings_list.index(string)
                 strings_list[index] = list_suggests[val]
                 print("index: {}".format(index))
             except ValueError:
-                print("No has elegido nada")
-    return new_sentence.join(strings_list)
+                strings_list[index] = value
+    return " ".join(strings_list)
                 
             
 
@@ -53,21 +87,24 @@ def clean_row(row):
 
 
 def main():
-    list_of_rows = []
-    clean_dataframe = pd.DataFrame()
     for col in cols:
         if col != "idPersona" and col != "sexo":
-            row =  row_x_row(df[col])
-            list_of_rows.append(row)
-            print("row nuevo: ",list_of_rows)
-    #classifier()
+            row_x_row(df[col], col)  #esto regresará data frame con palabras analizadas y limpias
+    new_df.to_csv(ruta_destino)
+    print("Fin de limpieza, comienza clasificación.")
+    classifier(new_df)
+    df_sustantivos.to_csv('C:/Users/Alex Isasi/Desktop/HUNSPELL/pruebas/sustantivos.csv')
+    df_verbos.to_csv('C:/Users/Alex Isasi/Desktop/HUNSPELL/pruebas/verbos.csv')
+    df_adjetivos.to_csv('C:/Users/Alex Isasi/Desktop/HUNSPELL/pruebas/adjetivos.csv')
+    df_adverbios.to_csv('C:/Users/Alex Isasi/Desktop/HUNSPELL/pruebas/adverbios.csv')
+
             
 
 if __name__ == "__main__":
     h = Hunspell('es_MX',hunspell_data_dir='C:/Users/Alex Isasi/AppData/Local/Programs/Python/Python37/Lib/site-packages/dictionaries')
-    #nlp = spacy.load("es_core_news_md")
+    nlp = spacy.load("es_core_news_md")
     ruta_origen = 'C:/Users/Alex Isasi/Documents/GitHub/EvaluacionCognitiva/Bases de datos/Primaria/PRIMARIA_TODO.csv'
-    ruta_destino = 'C:/Users/Alex Isasi/Documents/GitHub/EvaluacionCognitiva/Bases de datos/Primaria/conteo_clasificatorio_general.csv'
+    ruta_destino = 'C:/Users/Alex Isasi/Documents/GitHub/EvaluacionCognitiva/Bases de datos/Primaria/palabras_limpias.csv'
     print('Recopilando datos de: ',ruta_origen)
     df = pd.read_csv(ruta_origen, encoding='latin1') #documento con muestras, modificar valor del argumento encoding por si arroja error al inicio
     cols = [ #empieza prueba 1
@@ -102,4 +139,9 @@ if __name__ == "__main__":
         'que_significa_9', 
         'por_que_crees_que_si_o_que_no_es_posible_9'
     ]
+    new_df = pd.DataFrame()
+    df_sustantivos = pd.DataFrame()
+    df_verbos = pd.DataFrame()
+    df_adjetivos = pd.DataFrame()
+    df_adverbios = pd.DataFrame()
     main()
